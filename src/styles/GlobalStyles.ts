6730976683101929cf8a7805e9aa03ffb9d4123e
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-09-01 14:02:57 -0300

    feat: sharing styles with themeProvider of styled components

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle` 
+
+
+  body {
+  font-family: 'Be Vietnam Pro', 'Roboto', Arial, Helvetica, sans-serif;
+  }
+
+`;
+
+export default GlobalStyle;
---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  fonts: {
+    primary: "'Be Vietnam Pro', Arial,sans-serif",
+  },
+};
+
+export default theme;
